7db95841c62286ab9f32c6c7ed24bc4ed96c294b
Infezt <99657022+Infezt@users.noreply.github.com>
2022-02-14 17:10:11 +0300

    da

---
+++ lesson_1/code_1.py
@@ -0,0 +1,18 @@
+"""
+print(5>6)
+print(5<6)
+print(5==6)
+print(5>=6)
+print(5<=6)
+print(5!=6)
+"""
+
+#name = input("Введите ваше имя:\n")
+#print(f"11{name}")
+
+# age = int(input("Введите возраст:\n"))
+# print(age // 2)
+
+a = 0
+b = 5
+c = -9
